b7f4331eb7ec853f2c765cb1e716c6ad7c3a3f5d
Ashim Gupta <ashim.gupta@tredence.com>
2024-01-29 15:57:33 +0530

    youtube-clone beta ready

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import Comment from "./Comment";
+import { COMMENTS_API_URL } from "../utils/constants";
+
+
+const commentsData = []
+const CommentList = ({comments})=>{
+    return comments.map((comment, index)=>(
+        <div key={index}>
+            <Comment data={comment}/>
+            <div className="pl-5 border border-l-black ml-5">
+                <CommentList comments = {comment.replies} />
+            </div>
+         </div>
+    ))
+};
+
+const CommentsContainer = ({videoID})=>{
+    const [commentThreads,setCommentThreads] = useState([])
+
+    const makeCommentsDataList = ()=>{
+        console.log("inside make comment list")
+        // console.log(commentThreads)
+        commentThreads.map((comment)=>{
+            // console.log("mapping comments- ")
+            // console.log(comment)
+            const comment_replies = []
+            if(comment.snippet.totalReplyCount > 0){
+                comment.replies.comments.map((reply)=>{
+                    const replyObject = {
+                        name: reply.snippet.authorDisplayName,
+                        text: reply.snippet.textDisplay,
+                        userImgUrl: reply.snippet.authorProfileImageUrl,
+                        replies: []
+                    }
+                    comment_replies.push(replyObject)
+                })
+            }
+            const commentObject = {
+                name: comment.snippet.topLevelComment.snippet.authorDisplayName,
+                text: comment.snippet.topLevelComment.snippet.textDisplay,
+                userImgUrl: comment.snippet.topLevelComment.snippet.authorProfileImageUrl,
+                replies: comment_replies
+            }
+
+            console.log("comment object- ")
+            console.log(commentObject)
+
+            commentsData.push(commentObject)
+        })
+    }
+    useEffect(()=>{
+        console.log("commentThreads- ")
+        console.log(commentThreads)
+        makeCommentsDataList();
+    },[commentThreads])
+
+    const getCommentThreads = async()=>{
+        console.log("making api call to- "+COMMENTS_API_URL+videoID)
+        const data = await fetch(COMMENTS_API_URL+videoID)
+        const jsonData = await data.json()
+        // console.log(jsonData.items.filter((item) => item.snippet.totalReplyCount > 0))
+        setCommentThreads(jsonData.items)
+        // console.log("commentThreads- ")
+        // console.log(commentThreads)
+        // makeCommentsDataList();
+    }
+
+    useEffect(()=>{
+        getCommentThreads();
+    },[])
+    return (
+        <div className="m-5 p-2">
+            <h1 className="text-2xl font-bold">Comments</h1>
+            <CommentList comments = {commentsData}/>
+        </div>
+    )
+}
+
+export default CommentsContainer
---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+const API_KEY = "AIzaSyDttmfrkpWzZyxlWMJHzKeYAoAL-xcofj8"
+
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/"
+
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+API_KEY;
+
+export const VIDEO_DETAILS_URL = "https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,liveStreamingDetails&key="+API_KEY+"&id="
+
+export const CHANNEL_DETAILS_URL = "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&key="+API_KEY+"&id="
+
+export const YOUTUBE_SEARCH_API = PROXY_URL+"https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const COMMENTS_API_URL = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet,replies&maxResults=99&key="+API_KEY+"&videoId="
+
+export const get_YOUTUBE_SEARCH_URL = (searchParams) => {
+    const URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchParams}&key=${API_KEY}`;
+    return URL;
+}
